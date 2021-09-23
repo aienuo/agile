fe2d1301a02357a6480d25cbe9c5a48fc7eb6005
XinLau <xiaotaiye1996@gmail.com>
2021-09-23 17:50:16 +0800

    自动填充字段处理程序

--- src/main/java/com/imis/agile/interceptor/AutoFillFieldsMetaObjectHandler.java
+++ src/main/java/com/imis/agile/interceptor/AutoFillFieldsMetaObjectHandler.java
@@ -6,6 +6,7 @@ import com.imis.agile.constant.CommonConstant;
 import com.imis.agile.constant.enums.CommonResponseEnum;
 import com.imis.agile.util.AgileUtil;
 import com.imis.agile.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -16,7 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * <p>
  * AutoFillFieldsMetaObjectHandler<br>
- * 自动填充字段元对象处理程序<br>
+ * 自动填充字段处理程序<br>
  * 自动注入创建人、创建时间、修改人、修改时间
  * </p>
  *
@@ -24,6 +25,7 @@ import javax.servlet.http.HttpServletRequest;
  * @version 1.0
  * @since 2021年01月20日 13:24
  */
+@Slf4j
 @Component
 public class AutoFillFieldsMetaObjectHandler implements MetaObjectHandler {
 
@@ -54,8 +56,9 @@ public class AutoFillFieldsMetaObjectHandler implements MetaObjectHandler {
             // 获取 Token 中的 username
             username = JwtUtil.getUsername(token);
         }
-        this.setFieldValByName(com.imis.agile.constant.DataBaseConstant.CREATE_BY, username, metaObject);
-        this.setFieldValByName(com.imis.agile.constant.DataBaseConstant.CREATE_TIME, java.time.LocalDateTime.now(), metaObject);
+        log.debug("执行 插入数据时的填充策略");
+        this.strictInsertFill(metaObject, com.imis.agile.constant.DataBaseConstant.CREATE_BY, String.class, username);
+        this.strictInsertFill(metaObject, com.imis.agile.constant.DataBaseConstant.CREATE_TIME, java.time.LocalDateTime.class, java.time.LocalDateTime.now());
     }
 
     /**
@@ -78,8 +81,9 @@ public class AutoFillFieldsMetaObjectHandler implements MetaObjectHandler {
             // 获取 Token 中的 username
             username = JwtUtil.getUsername(token);
         }
-        this.setFieldValByName(com.imis.agile.constant.DataBaseConstant.UPDATE_BY, username, metaObject);
-        this.setFieldValByName(com.imis.agile.constant.DataBaseConstant.UPDATE_TIME, java.time.LocalDateTime.now(), metaObject);
+        log.debug("执行 更新数据时的填充策略");
+        this.strictUpdateFill(metaObject, com.imis.agile.constant.DataBaseConstant.UPDATE_BY, String.class, username);
+        this.strictUpdateFill(metaObject, com.imis.agile.constant.DataBaseConstant.UPDATE_TIME, java.time.LocalDateTime.class, java.time.LocalDateTime.now());
     }
 
 }
