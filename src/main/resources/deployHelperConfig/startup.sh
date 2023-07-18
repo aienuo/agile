42a9919685a3019bd648086ab61e15816714f512
XinLau <xiaotaiye1996@gmail.com>
2023-07-18 11:18:36 +0800

    优化 自动化部署脚本

--- src/main/resources/deployHelperConfig/startup.sh
+++ src/main/resources/deployHelperConfig/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-jarName=agile-1.0.jar
+jarName=agile-1.0.0.jar
 JVM_OPTS="-Dname=$jarName  -Duser.timezone=Asia/Shanghai -Xms512M -Xmx1024M -XX:PermSize=256M -XX:MaxPermSize=512M -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
 nohup java -jar  $JVM_OPTS /home/agile/dev/$jarName > /dev/null 2>&1 &
